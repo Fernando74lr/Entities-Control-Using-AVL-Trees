43de9cc3deffa643a63c01ff310ae7a0e87de20d
GUPABE <49884534+GUPABE@users.noreply.github.com>
2019-05-09 12:21:54 -0500

    Create Report.h

---
+++ Report.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "Entity.h"
+
+
+void Ascending_Order_Plate(struct client* node) 
+{ 
+     if (node == NULL) 
+          return; 
+  
+     /* first recur on left child */
+     Ascending_Order_Plate(node->left); 
+  
+     /* then print the data of node */
+    printf("---------------------------------------------\n");
+    printf("Placas: ");
+	print_string(node->plate);
+	printf("\nTipo de carro: ");
+	print_string(node->car_type);
+	printf("\nTipo de servicio: ");
+	print_string(node->service_type);
+	printf("\nMecanico asignado: ");
+	print_string(node->assigned_mechanic);
+	printf("\nTiempo requerido: %i \n\n", node->time_required); 
+	printf("---------------------------------------------\n");
+	printf("\n\n");
+  
+     /* now recur on right child */
+     Ascending_Order_Plate(node->right); 
+} 
+
+
+void Descending_Order_Plate(struct client* node) 
+{ 
+     if (node == NULL) 
+          return; 
+  
+     /* first recur on left child */
+     Descending_Order_Plate(node->right); 
+  
+     /* then print the data of node */
+    printf("---------------------------------------------\n");
+    printf("Placas: ");
+	print_string(node->plate);
+	printf("\nTipo de carro: ");
+	print_string(node->car_type);
+	printf("\nTipo de servicio: ");
+	print_string(node->service_type);
+	printf("\nMecanico asignado: ");
+	print_string(node->assigned_mechanic);
+	printf("\nTiempo requerido: %i \n\n", node->time_required); 
+	printf("---------------------------------------------\n");
+	printf("\n\n");
+  
+     /* now recur on right child */
+     Descending_Order_Plate(node->left); 
+} 
