9cd37b333db093c91c7f504201b01af0c6c0ffd4
Fernando74lr <39963559+Fernando74lr@users.noreply.github.com>
2019-05-09 18:21:37 -0500

    Update

--- Search.h
+++ Search.h
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 // #include "Entity.h"
-
+int flag = 0;
+int flag2 = 0;
 
 struct client* search(struct client* root, char* find) {
     // printf("buscando mas...\n");
@@ -18,4 +19,66 @@ struct client* search(struct client* root, char* find) {
     }
     else
         return search(root->left, find);
+}
+
+void higher_values(struct client* node, char * plate) {
+     if (node == NULL) 
+          return; 
+  
+    /* first recur on left child */
+    higher_values(node->left, plate);
+    if (strcmp(node->plate, plate) == 0){ 
+    	flag2 = 1;
+    }
+    if (flag2==1){
+	     /* then print the data of node */
+	    printf("---------------------------------------------\n");
+	    printf("Placas: ");
+		print_string_p(node->plate);
+		printf("\nTipo de carro: ");
+		print_string_p(node->car_type);
+		printf("\nTipo de servicio: ");
+		print_string_p(node->service_type);
+		printf("\nMecanico asignado: ");
+		print_string_p(node->assigned_mechanic);
+		printf("\nTiempo requerido: %i \n\n", node->time_required); 
+		printf("---------------------------------------------\n");
+		printf("\n\n");
+    }
+  
+     /* now recur on right child */
+     higher_values(node->right, plate); 
+}
+
+void less_values(struct client* node, char * plate) {
+     if (node == NULL) 
+          return; 
+  
+    /* first recur on left child */
+    less_values(node->left, plate);
+    if (flag==1){ 
+    	return;
+    }
+
+     /* then print the data of node */
+    printf("---------------------------------------------\n");
+    printf("Placas: ");
+	print_string(node->plate);
+	printf("\nTipo de carro: ");
+	print_string(node->car_type);
+	printf("\nTipo de servicio: ");
+	print_string(node->service_type);
+	printf("\nMecanico asignado: ");
+	print_string(node->assigned_mechanic);
+	printf("\nTiempo requerido: %i \n\n", node->time_required); 
+	printf("---------------------------------------------\n");
+	printf("\n\n");
+
+	if (strcmp(node->plate, plate) == 0 || flag==1){ 
+		flag = 1;
+		return;
+	}
+  
+     /* now recur on right child */
+     less_values(node->right, plate); 
 }
