f87591157282243bad252e32f4220b2ed766f619
Fernando74lr <39963559+Fernando74lr@users.noreply.github.com>
2019-05-09 15:31:47 -0500

    Updated method

--- Search.h
+++ Search.h
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "Entity.h"
+// #include "Entity.h"
 
 
-struct client* search(struct client* root, char* find){
-	printf("buscando mas...\n");
-	if(strcmp(root->plate,find) == 0){
-		return root;
-	}
-	else if(strcmp(root->plate,find) <0){
-		return search(root->right, find);
-	}
-	else if(strcmp(root->plate,find) >0)
-		return search(root->left, find);
-	else
-		printf("No encontrado\n");
-		return NULL;
+struct client* search(struct client* root, char* find) {
+    // printf("buscando mas...\n");
+    if (root == NULL) {
+        printf("No encontrado\n");
+        return NULL;
+    }
+    if(strcmp(root->plate,find) == 0){
+        return root;
+    }
+    else if(strcmp(root->plate,find) <0){
+        return search(root->right, find);
+    }
+    else
+        return search(root->left, find);
 }
-
